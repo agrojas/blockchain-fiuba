abc2a52d677a631e43780e3d497c90f59559c896
Agustin Rojas <your_email@abc.com>
2019-09-13 16:38:13 -0300

    Add new  changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {activo: 'TBD', value: '', message: ''};
+  }
+
+  async componentDidMount() {
+    // rxotx
+    const activo = await ABI.methods.activo().cal();
+    this.setState({activo})
+  }
+
+  onSubmit = async event => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- web3-playground.js
+++ web3-playground.js
@@ -1,22 +1,26 @@
+cte_model_buyer_1           | 2019-09-12 03:36:41,492 INFO - Finish model with status FINISHED
+cte_model_buyer_1           | 2019-09-12 03:36:41,495 ERROR - 'metrics'
+cte_model_buyer_1           | 2019-09-12 03:36:41,495 ERROR - Exception on /models/636e8f66-d50e-11e9-8246-0242ac120003 [PUT]
+cte_model_buyer_1           | Traceback (most recent call last):
+cte_model_buyer_1           |   File "/root/.local/lib/python3.7/site-packages/flask/app.py", line 1832, in full_dispatch_request
+cte_model_buyer_1           |     rv = self.dispatch_request()
+cte_model_buyer_1           |   File "/root/.local/lib/python3.7/site-packages/flask/app.py", line 1818, in dispatch_request
+cte_model_buyer_1           |     return self.view_functions[rule.endpoint](**req.view_args)
+cte_model_buyer_1           |   File "/root/.local/lib/python3.7/site-packages/flask_restplus/api.py", line 325, in wrapper
+cte_model_buyer_1           |     resp = resource(*args, **kwargs)
+cte_model_buyer_1           |   File "/root/.local/lib/python3.7/site-packages/flask/views.py", line 88, in view
+cte_model_buyer_1           |     return self.dispatch_request(*args, **kwargs)
+cte_model_buyer_1           |   File "/root/.local/lib/python3.7/site-packages/flask_restplus/resource.py", line 44, in dispatch_request
+cte_model_buyer_1           |     resp = meth(*args, **kwargs)
+cte_model_buyer_1           |   File "/app/model_buyer/resources/models_resource.py", line 123, in put
+cte_model_buyer_1           |     ModelBuyerService().finish_model(model_id, data)
+cte_model_buyer_1           |   File "/app/model_buyer/services/model_buyer_service.py", line 75, in finish_model
+cte_model_buyer_1           |     model = self._update_model(model_id, data, BuyerModelStatus.FINISHED.name)
+cte_model_buyer_1           |   File "/app/model_buyer/services/model_buyer_service.py", line 104, in _update_model
+cte_model_buyer_1           |     diffs = data['metrics']['diffs']
+cte_model_buyer_1           | KeyError: 'metrics'
 
-var Web3 = require('web3');
-var web3 = new Web3();
 
-// Conexion con el nodo  HTTP://127.0.0.1:7545
-web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
-
-web3.eth.getAccounts()
-.then(accounts => {
-    console.log("All accounts ", JSON.stringify(accounts, null))
-    return accounts;
-})
-.then(accounts => {
-    return web3.eth.getBalance(accounts[0])
-})
-.then(balance => {
-    console.log("balance in wei ", balance)
-    console.log("balance in ether", web3.utils.fromWei(balance, 'ether'))
-})
-.catch(error => {
-    console.log(error)
-})
+DO-10
+FA-12
+MB-30
